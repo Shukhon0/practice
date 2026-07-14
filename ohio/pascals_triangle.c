2ddfc4b53981f237531d8ef33197ab39b6531c76
Shukhon <68188121+Shukhon0@users.noreply.github.com>
2026-07-15 00:48:35 +0600

    e

--- ohio/pascals_triangle.c
+++ ohio/pascals_triangle.c
@@ -11,7 +11,7 @@ void main () {
     scanf("%d",&n);
     for (int i=0;i<=n;i++) {
         for (int j=0;j<=i;j++) {
-            for (int k=j;k<(n/2);k++) {
+            for (int k=j;k<=(n/2);k++) {
                 printf(" ");
             }
             printf("%d ",ncr(i,j));
