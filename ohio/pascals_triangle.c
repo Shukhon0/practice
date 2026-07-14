a21b1760c5f1e93f7e80fb226445faf951ae07c2
Shukhon <68188121+Shukhon0@users.noreply.github.com>
2026-07-15 00:27:09 +0600

    e

--- ohio/pascals_triangle.c
+++ ohio/pascals_triangle.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 int n;
+int factorial (int x) {
+    return factorial(x-1)*x;
+};
+int ncr(int a,int b) {
+    return factorial(a)/(factorial(b)*factorial(a-b));
+};
 void main () {
     scanf("%d",&n);
-    for (;;) {
-        for (;;) {
-            printf("%d ");
+    for (int i=0;i<n;i++) {
+        for (int j;j<i;j++) {
+            printf("%d ",ncr);
         }
+        printf("\n");
     }
 }
