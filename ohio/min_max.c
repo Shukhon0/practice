5cbeadfeaee4f3fba13c6a239ab22422829648d7
Shukhon <68188121+Shukhon0@users.noreply.github.com>
2026-07-15 22:30:32 +0600

    e

--- ohio/min_max.c
+++ ohio/min_max.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int min () {
-
+int n;
+int min (int x[]) {
+    for (int i=0;i<n;i++) {
+        if (x[i]<x[i+1]) {
+            return min(x[i]);
+        }
+    }
 };
 int max () {
     
