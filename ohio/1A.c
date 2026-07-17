9c4827913e4c396ba7c6fc562ccfc97fbed20051
Shukhon <68188121+Shukhon0@users.noreply.github.com>
2026-07-18 00:18:24 +0600

    e

--- ohio/1A.c
+++ ohio/1A.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main () {
     long long int n,m,a;
-    scanf("%lld %lld %lld",n,m,a);
+    if(scanf("%lld %lld %lld",n,m,a)!=3) return 1; else {
     printf("%lld",(-n/a)*(-m/a));
     return 0;
+    };
 }
