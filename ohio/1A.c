dd9dbab187f11de426d7de4cc49c4fb15b27dbfe
Shukhon <68188121+Shukhon0@users.noreply.github.com>
2026-07-18 00:24:33 +0600

    e

--- ohio/1A.c
+++ ohio/1A.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main () {
     long long int n,m,a;
-    if(scanf("%lld %lld %lld",n,m,a)!=3) return 1; else {
-    printf("%lld",(-n/a)*(-m/a));
+if (scanf("%lld %lld %lld", &n, &m, &a) != 3 && n>=1 && m >=1 && a>=1 && a<= 1000000000) {
+    return 1;
+}   
+    else {
+    printf("%lld",((n+a-1)/a)*((m+a-1)/a));
     return 0;
-    };
+    }
 }
