cf3b20727d3ace7c744aa6046e37ce67023744b2
Shukhon <68188121+Shukhon0@users.noreply.github.com>
2026-07-19 01:06:46 +0600

    e

--- ohio/agegap.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-void main () {
-    int leapyearcount,dd1,mm1,yyyy1,dd2,mm2,yyyy2;
-    scanf("%d/%d/%d",&dd1,&mm1,&yyyy1);
-    scanf("%d/%d/%d",&dd2,&mm2,&yyyy2);
-    
-}
---
+++ ohio/if1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main () {
+    int a=1,num=2;
+    if (a!=0 && num/a){
+        printf("sigma");
+    }
+}
