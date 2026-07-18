de1fd4dcd472b72bb17affb57f17602508f4f58e
Shukhon <68188121+Shukhon0@users.noreply.github.com>
2026-07-19 00:51:16 +0600

    e

--- ohio/swap.c
+++ ohio/swap.c
@@ -2,8 +2,8 @@
 void main () {
     int a,b;
     scanf("%d %d",a,b);
-    a=a+b;
-    b=a-b;
-    a=a-b;
+    a=a+b; /*a to a+b*/
+    b=a-b; //b to a
+    a=a-b; //a to b
     printf("%d %d",a,b);
 }
